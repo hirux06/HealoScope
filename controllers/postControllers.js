8f404f5e3b117bf06e00a59025821a79aa64daf9
Saran Hiruthik M <saran.hiruthik83@gmail.com>
2025-01-12 21:52:10 +0530

    added backend files (postController routing functions written)

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,31 +1,98 @@
 import Post from "../models/postModel.js";
 import User from "../models/userModel.js";
+import moment from "moment-timezone";
 
 
 const postController = {
     createPost: async (req, res) => {
-        const { id: _id, title, tags , body } = req.body;
+        const { id, title, tags , body } = req.body;
         try {
-            const user = User.findOne({ id });
+            const user = await User.findOne({ _id: id });
+
 
             if (!user) {
                 return res.status(400).json({ message: "User not found!!" });
             }
 
             if (user.role !== "doctor") {
-                return res.status(403).json({ message: "Only doctors can create posts" });
+                return res.status(403).json({ message: "Only doctors can create posts", user: user.role });
             }
 
             const newPost = new Post({
-                id, 
+                userId: id, 
                 title,
                 body,
                 tags
             });
             await newPost.save();
-                res.status(201).json({ message: "Post created successfully", post: newPost, user });
+                res.status(201).json({ message: "Post created successfully", post: newPost});
         } catch (error) {
             res.status(500).json({ message: "Error creating post", error: error.message });
+        }
+    },
+
+    getAllPost: async (req, res) => {
+        try {
+            const posts = await Post.find().populate('userId', 'name username profilePicture');
+            res.status(200).json({ message: "Posts fetched successfully", posts });
+        } catch (error) {
+            res.status(500).json({ message: "Error fetching posts", error: error.message });
+        }
+    },
+
+    getPostById: async (req, res) => {
+        const {id} = req.params;
+
+        try {
+            const posts = await Post.findById(id).populate('userId', 'name username profilePicture');
+            res.status(200).json({ message: "Posts fetched successfully", posts });
+        } catch (error) {
+            res.status(500).json({ message: "Error fetching posts", error: error.message });
+        }
+    },
+
+    editPost: async (req, res) => {
+        const {id} = req.params;
+        const { title, body, tags, media } = req.body;
+
+        try{
+            const post = await Post.findById(id);
+
+
+            if(!post) {
+                res.status(400).json({message: "Post not found"});
+            }
+
+            post.title = title || post.title;
+            post.body = body || post.body;
+            post.tags = tags || post.tags;
+            post.media = media || post.media;
+            post.updatedAt = moment().tz("Asia/Kolkata").format("DD-MM-YYYY HH:mm:ss");
+
+            const updatedPost = await post.save();
+
+            console.log(updatedPost);
+            res.status(200).json({ message: "Post updated successfully", updatedPost });
+            } catch (error) {
+                res.status(500).json({ message: "Error updating post", error: error.message });
+            }
+    },
+
+    deletePost: async (req, res) => {
+        const {id} = req.params;
+
+        try{
+            const post = await Post.findById(id);
+
+
+            if(!post) {
+                res.status(400).json({message: "Post not found"});
+            }
+
+            await Post.findByIdAndDelete(id);
+            res.status(200).json({ message: "Post deleted successfully" });
+        } catch (error) {
+            res.status(500).json({ message: "Error updating post", error: error.message });
         }
     }
 };
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -3,6 +3,11 @@ import postController from "../controllers/postControllers.js";
 
 const postRouter = Router();
 
-postRouter.post("/create", postController.createPost);
+postRouter.post("/createPost", postController.createPost);
+postRouter.get("/allPosts", postController.getAllPost);
+postRouter.get("/viewPost/:id", postController.getPostById);
+postRouter.put("/editPost/:id", postController.editPost );
+postRouter.delete("/deletePost/:id", postController.deletePost );
+
 
 export default postRouter;
